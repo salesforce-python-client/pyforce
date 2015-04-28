8f0e08d3ac96776891f511f8844f83c2da97bfff
Alan Castonguay <acastonguay@vmware.com>
2015-04-27 20:16:34 -0400

    v1.6 - Introduce logout functionality, New maintainer
    Add support for the `logout` interface.
    Bump version to 1.6.
    Add new maintainer contact.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 setup(name='pyforce',
-    version='1.4', # be sure to update the version in pyforce.py too
+    version='1.6', # be sure to update the version in pyforce.py too
     package_dir={'': 'src'},
     packages=['pyforce'],
     author = "Simon Fell et al.  reluctantly Forked by idbentley",
-    author_email = 'ian.bentley@gmail.com',
-    description = "A Python client wrapping the Saleforce.com SOAP API",
+    author_email = 'ian.bentley@gmail.com, alanjcastonguay@gmail.com',
+    description = "A Python client wrapping the Salesforce.com SOAP API",
     long_description = open('README.md').read() + "\n" + open('CHANGES.txt').read(),
     license = "GNU GENERAL PUBLIC LICENSE Version 2",
     keywords = "python salesforce salesforce.com",
